2e32b1b31f1282d3923b0e2599ee342e43acc69b
Harkirat Singh <harkirat96@gmail.com>
2024-01-09 22:27:27 +0530

    right screenshare

--- backend/src/managers/RoomManager.ts
+++ backend/src/managers/RoomManager.ts
@@ -1,4 +1,4 @@
-import { User } from "./UserManger";
+ import { User } from "./UserManger";
 
 let GLOBAL_ROOM_ID = 1;
 
@@ -14,7 +14,7 @@ export class RoomManager {
     }
 
     createRoom(user1: User, user2: User) {
-        const roomId = this.generate();
+        const roomId = this.generate().toString();
         this.rooms.set(roomId.toString(), {
             user1, 
             user2,
@@ -23,19 +23,27 @@ export class RoomManager {
         user1.socket.emit("send-offer", {
             roomId
         })
+
+        user2.socket.emit("send-offer", {
+            roomId
+        })
     }
 
     onOffer(roomId: string, sdp: string) {
         const user2 = this.rooms.get(roomId)?.user2;
+        console.log("user2 is " + user2);
         user2?.socket.emit("offer", {
-            sdp
+            sdp,
+            roomId
         })
     }
     
     onAnswer(roomId: string, sdp: string) {
         const user1 = this.rooms.get(roomId)?.user1;
-        user1?.socket.emit("offer", {
-            sdp
+        console.log("user1 is " + user1);
+        user1?.socket.emit("answer", {
+            sdp,
+            roomId
         });
     }
 
--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,15 +1,84 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { Socket, io } from "socket.io-client";
+
+const URL = "http://localhost:3000";
 
 export const Room = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const name = searchParams.get('name');
+    const [lobby, setLobby] = useState(true);
+    const [socket, setSocket] = useState<null | Socket>(null);
+    const [sendingPc, setSendingPc] = useState<null | RTCPeerConnection>(null);
+    const [receivingPc, setReceivingPc] = useState<null | RTCPeerConnection>(null);
+    const [remoteVideoTrack, setRemoteVideoTrack] = useState<MediaStreamTrack | null>(null);
+    const [localVideoTracck, setlocalVideoTracck] = useState<MediaStreamTrack, null>(null);
+    const [remoteAudioTrack, setRemoteAudioTrack] = useState<MediaStreamTrack | null>(null);
+    const [localAudioTrack, setLocalAudioTrack] = useState<MediaStreamTrack, null>(null);
 
     useEffect(() => {
-        // logic to init user to the room
+        const socket = io(URL);
+        socket.on('send-offer', async ({roomId}) => {
+            setLobby(false);
+            const pc = new RTCPeerConnection();
+            setSendingPc(pc);
+
+            const sdp = await pc.createOffer();
+            socket.emit("offer", {
+                sdp,
+                roomId
+            })
+        });
+
+        socket.on("offer", async ({roomId, offer}) => {
+            setLobby(false);
+            const pc = new RTCPeerConnection();
+            pc.setRemoteDescription({sdp: offer, type: "offer"})
+            const sdp = await pc.createAnswer();
+            // trickle ice 
+            setReceivingPc(pc);
+            pc.ontrack = (({track, type}) => {
+                if (type == 'audio') {
+                    setRemoteAudioTrack(track);
+                } else {
+                    setRemoteVideoTrack(track);
+                }
+            })
+            socket.emit("answer", {
+                roomId,
+                sdp: sdp
+            });
+        });
+
+        socket.on("answer", ({roomId, answer}) => {
+            setLobby(false);
+            setSendingPc(pc => {
+                pc?.setRemoteDescription({
+                    type: "answer",
+                    sdp: answer
+                })
+                return pc;
+            })
+        })
+
+        socket.on("lobby", () => {
+            setLobby(true);
+        })
+
+        setSocket(socket)
     }, [name])
+
+    if (lobby) {
+        return <div>
+
+            Waiting to connect you to someone
+        </div>
+    }
 
     return <div>
         Hi {name}
+        <video width={400} height={400} />
+        <video width={400} height={400} />
     </div>
 }
+
