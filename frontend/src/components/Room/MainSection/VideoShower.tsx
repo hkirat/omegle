3af133159d876f511effc9a44e8ab9d979bdf37f
its-id <ik.dana24@gmail.com>
2024-01-09 23:18:58 +0530

    add video tags in VideoShower element

--- frontend/src/components/Room/MainSection/VideoShower.tsx
+++ frontend/src/components/Room/MainSection/VideoShower.tsx
@@ -1,8 +1,12 @@
 const VideoShower = () => {
   return (
     <div className='flex flex-col gap-16'>
-      <div className='w-[35vw] h-[35vh] border bg-gray-500 rounded-lg shadow'></div>
-      <div className='w-[35vw] h-[35vh] border bg-gray-500 rounded-lg shadow'></div>
+      <div className='w-[35vw] h-[35vh] border rounded-lg shadow'>
+        <video width={400} height={200} />
+      </div>
+      <div className='w-[35vw] h-[35vh] border rounded-lg shadow'>
+        <video width={400} height={200} />
+      </div>
     </div>
   );
 };
