21b7d0196753d394ffe05b36ab3256be31795254
Jack Whitman <naveen9nj@gmail.com>
2024-02-19 03:26:22 +0530

    pc.ontrack fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,11 @@
 import { Socket } from "socket.io";
 import http from "http";
 
-import express from 'express';
+// import express from 'express';
 import { Server } from 'socket.io';
 import { UserManager } from "./managers/UserManger";
 
-const app = express();
+// const app = express();
 const server = http.createServer(http);
 
 const io = new Server(server, {
